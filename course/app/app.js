19df5f66684eb6efb3670f0fb17ece3a005d7b11
Rafael Barros <rafaelbarros.df@gmail.com>
2018-04-09 01:25:29 -0300

    Service Constant

--- course/app/app.js
+++ course/app/app.js
@@ -1,6 +1,7 @@
 require('angular');
 require('./locale/angular-locale_pt-br')
 
+var configConstant = require('./config/configConstant');
 var configValue = require('./config/configValue');
 var configBonusProvider = require('./config/configBonusProvider');
 var bonusGenerator = require('./services/bonusGenerator');
@@ -11,9 +12,10 @@ var maskTel = require('./directives/maskTel');
 var alertMsg = require('./directives/alertMsg');
 
 angular.module('app', []);
+angular.module('app').constant('configConstant', configConstant);
 angular.module('app').value('configValue', configValue);
 angular.module('app').provider('bonusGenerator',[bonusGenerator]);
-angular.module('app').config(['bonusGeneratorProvider',configBonusProvider]);
+angular.module('app').config(['bonusGeneratorProvider','configConstant', configBonusProvider]);
 angular.module('app').factory('clientAPIService',['$http','configValue',clientAPIService]);
 angular.module('app').service('clientTestService',['$http','configValue', clientTestService]);
 angular.module('app').directive('maskTel',[maskTel]);
--- course/app/config/configBonusProvider.js
+++ course/app/config/configBonusProvider.js
@@ -1,4 +1,5 @@
-module.exports = function(bonusGeneratorProvider){
+module.exports = function(bonusGeneratorProvider, configConstant){
     console.log(bonusGeneratorProvider.getLength());
     bonusGeneratorProvider.setLength(20);
+    console.log(configConstant);
 };
